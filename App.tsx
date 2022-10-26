c3bc29f72bbefe0472dbac2580f63c2b635a546f
Tatiana Muñoz <tatiana.munoz.romeo@gmail.com>
2022-10-26 17:57:18 +0200

    Inicio proyecto

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScreenCalculator } from "./src/ui/_screens/ScreenCalculator";
+
+const Calculator = () => {
+  return (
+    <ScreenCalculator/>
+  );
+};
+
+export default Calculator;
---
+++ src/ui/_screens/ScreenCalculator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const ScreenCalculator = () => {
+  return (
+    <View>
+      <Text>CalculatorScreen</Text>
+    </View>
+  );
+};
+
